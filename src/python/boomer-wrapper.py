7e69222394ef73c6c54c28ec7a0ab02a2d703a4f
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-08-30 16:52:02 -0400

    Don't hardcode the list of algorithms

--- src/python/boomer-wrapper.py
+++ src/python/boomer-wrapper.py
@@ -1,11 +1,7 @@
-from browser import document, alert
-from boomer.api import boomer
+import boomer.api
+import browser
 
-__algorithms = {
-    'monique', 'alain', 'nicole', 'serge',
-    'andré', 'muriel', 'denis', 'guy', 'chantal',
-    'marc', 'manon', 'sylvain', 'josey', 'yves',
-}
+__algorithms = list(boomer.api._default_algo_cfgs().keys())
 
 def __list_algorithms():
     return __algorithms
@@ -14,15 +10,14 @@ def __execute(input, js_algo_configs=None, seed=None):
     # convert javascript objects to python objects
     algo_configs = {}
     if js_algo_configs is not None:
-        print("here")
         for algo_name in js_algo_configs:
             if algo_name not in __algorithms:
                 print('Invalid algorithm specified: ' + algo_name)
                 continue
             algo_configs[algo_name] = js_algo_configs[algo_name]
 
-    return boomer(input, algo_cfgs=algo_configs, seed=seed)
+    return boomer.boomer(input, algo_cfgs=algo_configs, seed=seed)
 
-document.boomer = {}
-document.boomer.algorithms = __list_algorithms
-document.boomer.execute = __execute
+browser.document.boomer = {}
+browser.document.boomer.algorithms = __list_algorithms
+browser.document.boomer.execute = __execute
