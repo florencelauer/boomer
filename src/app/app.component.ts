218df41f4af399a00967b5843913e11a14a3d50f
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-08-30 16:17:55 -0400

    Add temporary example usage of Boomer service
    This adds two text areas: an input and an output to the
    the boomer service. This is only temporary. We still need
    to add settings and a prettier interface.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BoomerService } from './boomer.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  textInput: string = "Bonjour, je m'appel Gabriel.";
+  textOutput: string = "";
+
+  constructor(private boomerService: BoomerService) {}
+
+  execute() {
+    this.textOutput = this.boomerService.transformText(this.textInput);
+  }
 }
