f858ad3fca6efd91e4b6ff3266182fb1a99fcdd1
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-08-31 09:13:27 -0400

    Add hardcoded description of each algorithm in Boomer service

--- src/app/boomer.service.ts
+++ src/app/boomer.service.ts
@@ -11,6 +11,8 @@ export class BoomerService {
     this.scriptLoadedReject  = reject;
   });
 
+  private descriptions: Map<string, string> = new Map();
+
   constructor() {
     /* set promise callbacks that are called from python script */
     (document as any).boomer = {};
@@ -22,6 +24,22 @@ export class BoomerService {
     script.src = 'python/boomer-wrapper.py';
     script.type = 'text/python';
     document.getElementsByTagName('head')[0].appendChild(script);
+
+    /* FIXME: ideally, this would be given by the Boomer python library. */ 
+    this.descriptions['monique'] = 'Nicole gère tout ce qui concerne la conjugaison des verbes du premier groupe.';
+    this.descriptions['alain']   = 'Alain permute certains suffixes.';
+    this.descriptions['nicole']  = 'Nicole gère tout ce qui concerne la conjugaison des verbes du premier groupe.';
+    this.descriptions['serge']   = 'Serge remplace des formes contractées par leur forme longue.';
+    this.descriptions['andré']   = 'André fait commencer certains mots par une majuscule.';
+    this.descriptions['muriel']  = 'Muriel fait commencer certains mots par une minuscule.';
+    this.descriptions['denis']   = 'Denis allonge certaines ponctuations.';
+    this.descriptions['guy']     = 'Guy supprime des accents.';
+    this.descriptions['chantal'] = 'Chantal remplace les apostrophes et les traits d\'union par des espaces ou par rien.';
+    this.descriptions['marc']    = 'Marc supprime des petits mots.';
+    this.descriptions['manon']   = 'Manon permute deux lettres d\'un mot assez long.';
+    this.descriptions['sylvain'] = 'Sylvain multiplie les espaces.';
+    this.descriptions['josey']   = 'Josey ajoute des virgules ou des points.';
+    this.descriptions['yves']    = 'Yves remplace bêtement certaines chaines par d\'autres qui sont phonétiquement équivalentes.';
   }
 
   async listAlgorithms(): Promise<string[]> {
@@ -40,5 +58,15 @@ export class BoomerService {
         return (document as any).boomer.execute(text, algos, seed);
       }
     }
+  }
+
+  async getDescription(name: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      if (this.descriptions.has(name)) {
+        return resolve(this.descriptions[name]);
+      }
+
+      reject();
+    })
   }
 }
