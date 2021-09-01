b7f98ad17d114b91ac1d708c833f33f445ffb689
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-09-01 10:49:54 -0400

    Correctly convert Javascript maps into Python maps

--- src/python/boomer-wrapper.py
+++ src/python/boomer-wrapper.py
@@ -10,13 +10,15 @@ def __execute(input, js_algo_configs=None, seed=None):
     # convert javascript objects to python objects
     algo_configs = {}
     if js_algo_configs is not None:
-        for algo_name in js_algo_configs:
+        for algo_name in js_algo_configs.keys():
             if algo_name not in __algorithms:
-                print('Invalid algorithm specified: ' + algo_name)
                 continue
-            algo_configs[algo_name] = js_algo_configs[algo_name]
+            algo_configs[algo_name] = js_algo_configs.get(algo_name)
 
-    return boomer.boomer(input, algo_cfgs=algo_configs, seed=seed)
+    if len(algo_configs) == 0:
+        algo_configs = None
+
+    return boomer.api.boomer(input, algo_cfgs=algo_configs, seed=seed)
 
 browser.document.boomer.algorithms = __list_algorithms
 browser.document.boomer.execute = __execute
